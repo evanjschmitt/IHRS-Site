e461a2b36581d7711fb85babf6a8072ab7cbab97
evanjschmitt <evanjschmitt@gmail.com>
2023-04-26 00:01:13 -0400

    Day 5 Part Two
    -Added New Favicon
    -Changed Some Pages
    -Doing Good :D

--- services/services.js
+++ services/services.js
@@ -1,4 +1,4 @@
-import { RABBITS } from "../hugeArray.js";
+import { RABBITS, SANCTUARY } from "../rabbitArrays.js";
 
 export function changeRoutes() {
   let hashTag = window.location.hash;
@@ -8,26 +8,30 @@ export function changeRoutes() {
     $.get(`pages/${pageID}.html`, function (data) {
       // console.log("data" + data);
       $("#app").html(data);
+      $('html, body').animate({scrollTop: '0px'}, 0);
       if (pageID == "adoptables") {
-        loop();
+        adoptableLoop();
+      }
+      if (pageID == "sanctuary") {
+        sanctuaryLoop();
       }
     });
   } else {
-    $.get("home.html", function (data) {
+    $.get("pages/home.html", function (data) {
       // console.log("data" + data);
       $("#app").html(data);
     });
   }
 }
 
-export function loop() {
+export function adoptableLoop() {
   for (let i = 0; i < RABBITS.length; i++) {
     let rabbit = RABBITS[i];
-    console.log(rabbit.name);
+    // console.log(rabbit.name);
     $("#adopt").append(`<div class="profile">
       <div class="left">
         <h3>${rabbit.name}</h3>
-        <img src="${rabbit.image}" alt="">
+        <img src="${rabbit.image}" alt="${rabbit.name}'s Profile Picture">
         <div class="info">
           <p><span>Breed:</span> ${rabbit.breed}</p>
           <p><span>Sex:</span> ${rabbit.sex}</p>
@@ -42,6 +46,34 @@ export function loop() {
           <p>${rabbit.bonded}</p>
           <p>${rabbit.RAC}</p>
         </div>
+          <p>${rabbit.bio}</p>
+        </div>
+        <div class="bioButton">
+          <a href="#rabbitBio"><span>View My Bio!</span></a>
+        </div>
+      </div>
+    </div>`);
+  }
+}
+
+export function sanctuaryLoop() {
+  for (let i = 0; i < SANCTUARY.length; i++) {
+    let rabbit = SANCTUARY[i];
+    // console.log(rabbit.name);
+    $("#sanctuary").append(`<div class="profile">
+      <div class="left">
+        <h3>${rabbit.name}</h3>
+        <img src="${rabbit.image}" alt="${rabbit.name}'s Profile Picture">
+        <div class="info">
+          <p><span>Breed:</span> ${rabbit.breed}</p>
+          <p><span>Sex:</span> ${rabbit.sex}</p>
+          <p><span>Age:</span> ${rabbit.age}</p>
+          <p><span>Weight:</span> ${rabbit.weight}</p>
+        </div>
+  
+      </div>
+      <div class="right">
+        <div class="bio">
           <p>${rabbit.bio}</p>
         </div>
         <div class="bioButton">
